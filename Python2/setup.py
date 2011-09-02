9f50abdb8d132257201258eb654c9381206f56ae
geospatialpython@gmail.com <geospatialpython@gmail.com@6bd14dac-74e7-11de-ab85-01204adfcb19>
2011-09-02 19:23:07 +0000

    - added setuptools setup.py - updated usage.py
    git-svn-id: https://pyshp.googlecode.com/svn/trunk@36 6bd14dac-74e7-11de-ab85-01204adfcb19

---
+++ Python2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import usage
+
+setup(name='pyshp',
+      version='1.0.5',
+      description='Pure Python read/write support for ESRI Shapefile format',
+      long_description=usage.__doc__.strip(),
+      author='Joel Lawhead',
+      author_email='jlawhead@geospatialpython.com',
+      url='http://code.google.com/p/pyshp',
+      py_modules=['shapefile','usage'],
+      license='MIT',
+      zip_safe=False,
+      keywords='gis geospatial geographic shapefile shapefiles',
+      classifiers=['Programming Language :: Python',
+                   'Topic :: Scientific/Engineering :: GIS',
+                   'Topic :: Software Development :: Libraries',
+                   'Topic :: Software Development :: Libraries :: Python Modules'])
