92f275b65b0efcf3048e92d53fa911575c5f509d
Karim Bahgat <karim.bahgat.norway@gmail.com>
2018-11-07 00:46:11 -0500

    Support markdown on PyPI

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 def read_file(file):
     with open(file, 'rb') as fh:
         data = fh.read()
-
     return data.decode('utf-8')
 
 setup(name='pyshp',
       version='2.0.0',
       description='Pure Python read/write support for ESRI Shapefile format',
       long_description=read_file('README.md'),
+      long_description_content_type='text/markdown',
       author='Joel Lawhead',
       author_email='jlawhead@geospatialpython.com',
       url='https://github.com/GeospatialPython/pyshp',
