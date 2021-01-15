bff30d0250813304d037ecfd840d0738f440c3a7
Miroslav Šedivý <6774676+eumiro@users.noreply.github.com>
2021-01-15 08:27:05 +0100

    Add Python 3.9 support

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(name='pyshp',
                    'Programming Language :: Python :: 3.6',
                    'Programming Language :: Python :: 3.7',
                    'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3.9',
                    'Topic :: Scientific/Engineering :: GIS',
                    'Topic :: Software Development :: Libraries',
                    'Topic :: Software Development :: Libraries :: Python Modules'])
