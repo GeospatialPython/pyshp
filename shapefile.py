74cf6dd96ef7c7d1fb4b64b14318eef03342ddc4
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-10-17 21:20:12 +0100

    Let Ruff adjust code style (lead with ands, instead of trailing).

--- shapefile.py
+++ shapefile.py
@@ -855,15 +855,13 @@ class _Record(list):
         return default + fnames
 
     def __eq__(self, other):
-        return (isinstance(other, self.__class__) and
-                self.__field_positions == other.__field_positions and
-                self.oid == other.oid and
-                len(self) == len(other) and
-                all(val_self == val_other
-                    for val_self, val_other in izip(self, other)
-                   )
-               )
-
+        return (
+            isinstance(other, self.__class__)
+            and self.__field_positions == other.__field_positions
+            and self.oid == other.oid
+            and len(self) == len(other)
+            and all(val_self == val_other for val_self, val_other in izip(self, other))
+        )
 
 
 class ShapeRecord(object):
